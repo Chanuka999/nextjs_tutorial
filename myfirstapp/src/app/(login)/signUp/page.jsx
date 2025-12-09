dc4de83fd4e161db33e6de8d4fc43c49acb38725
Chanuka999 <chanukaranditha99@gmail.com>
2025-12-09 09:07:55 +0530

    first add

---
+++ myfirstapp/src/app/(login)/signUp/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <h1>sign up page</h1>;
+};
+
+export default page;
---
+++ myfirstapp/src/app/services/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <h1>Services page</h1>;
+};
+
+export default page;
