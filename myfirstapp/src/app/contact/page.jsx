06e3b3b35c946182a5fe0989b3722a3f9e22ff9e
Chanuka999 <chanukaranditha99@gmail.com>
2025-12-11 13:44:29 +0530

    Add contact page with navigation link to home

--- myfirstapp/src/app/contact/page.jsx
+++ myfirstapp/src/app/contact/page.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import styles from "./contact.module.css";
+import Link from "next/link";
 
 const page = () => {
-  return <h1>Contact us page</h1>;
+  return (
+    <>
+      <h1>Contact us page</h1>
+      <Link href="/" className={styles.Link}>
+        Home
+      </Link>
+    </>
+  );
 };
 
 export default page;
