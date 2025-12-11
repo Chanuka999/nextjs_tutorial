6a4ed949e022eeabd8df9de29d0cc9111312e7d9
Chanuka999 <chanukaranditha99@gmail.com>
2025-12-11 13:44:12 +0530

    Add navigation links and external site link to home page

--- myfirstapp/src/app/page.jsx
+++ myfirstapp/src/app/page.jsx
@@ -1,10 +1,55 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       <h1>Home page</h1>
+      <ul>
+        <li>
+          <Link href="/" className={styles.Link}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link href="/services" className={styles.Link}>
+            Services
+          </Link>
+        </li>
+        <li>
+          <Link href="/product" className={styles.Link}>
+            Product
+          </Link>
+        </li>
+        <li>
+          <Link href="/about" className={styles.Link}>
+            About
+          </Link>
+        </li>
+        <li>
+          <Link href="/contact" className={styles.Link}>
+            Contact us
+          </Link>
+        </li>
+        <li>
+          <Link href="/about/branches" className={styles.Link}>
+            Branches
+          </Link>
+        </li>
+        <li>
+          <Link href="/product/vegis" className={styles.Link}>
+            vegis
+          </Link>
+        </li>
+      </ul>
+      <Link
+        href="https://my-portfolio-jc4a.vercel.app/"
+        target="_blank"
+        id={styles.specialLink}
+      >
+        My site
+      </Link>
     </>
   );
 }
