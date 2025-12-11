74234580307ef330f371730c049114e46a418e33
Chanuka999 <chanukaranditha99@gmail.com>
2025-12-11 13:44:44 +0530

    Add services page with navigation link to home

--- myfirstapp/src/app/services/page.jsx
+++ myfirstapp/src/app/services/page.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Link from "next/link";
 
 const page = () => {
-  return <h1>Services page</h1>;
+  return (
+    <>
+      <h1>Services page</h1>
+      <button>
+        <Link href="/">Back to home</Link>
+      </button>
+    </>
+  );
 };
 
 export default page;
