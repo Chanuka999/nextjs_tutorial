8db89c3adcbe8d115470e51284fea1ef3fc8c0c9
Chanuka999 <chanukaranditha99@gmail.com>
2025-12-11 13:44:36 +0530

    Add vegetable listing with links to individual items

--- myfirstapp/src/app/product/vegis/[vegid]/page.jsx
+++ myfirstapp/src/app/product/vegis/[vegid]/page.jsx
@@ -1,8 +1,18 @@
+import Link from "next/link";
 import React from "react";
 
 const page = async ({ params }) => {
   const { vegid } = await params;
-  return <div>vegi item {vegid}</div>;
+  return (
+    <>
+      <div>vegi item {vegid}</div>
+      <div>
+        <button>
+          <Link href="/product/vegis">Back to</Link>
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default page;
--- myfirstapp/src/app/product/vegis/page.jsx
+++ myfirstapp/src/app/product/vegis/page.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import Link from "next/link";
 
 const page = () => {
-  return <div>vegitble page</div>;
+  const vegis = [
+    {
+      id: 1,
+      name: "tomata",
+    },
+    {
+      id: 2,
+      name: "spinach",
+    },
+    {
+      id: 3,
+      name: "mushroom",
+    },
+    {
+      id: 4,
+      name: "garlic",
+    },
+    {
+      id: 5,
+      name: "corn",
+    },
+  ];
+  return (
+    <div>
+      vegitble page
+      <ul>
+        {vegis.map((vegi, id) => {
+          return (
+            <li key={id}>
+              <Link href={`/product/vegis/${vegi.name}`}>{vegi.name}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default page;
